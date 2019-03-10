03b3c7c5e02fd5ab9fc31f0df397cb8da63e16ae
rashfael <rashfael@isobeef.org>
2019-03-10 17:10:21 +0100

    better animations, starting on speaker notes

--- src/daumenkino.vue
+++ src/daumenkino.vue
@@ -1,14 +1,10 @@
 <template lang="pug">
-.daumenkino(:class="[`daumenkino-theme-${theme}`]", :style="style")
-	.slides(ref="slides", :style="slidesStyle")
-		slot
+.daumenkino(:class="[`daumenkino-theme-${theme}`, {'speaker-mode': speakerMode}]", :style="style")
+	slot
 	.progress-bar-rail
 		.progress-bar
 </template>
 <script>
-const SLIDE_WIDTH = 960
-const SLIDE_HEIGHT = 700
-
 export default {
 	components: {},
 	props: {
@@ -22,12 +18,17 @@ export default {
 			animating: false,
 			slides: [],
 			activeIndex: 0,
-			slideScale: 1
+			slideScale: 1,
+			speakerMode: false,
+			transitionDirection: ''
 		}
 	},
 	computed: {
 		activeSlide () {
 			return this.slides[this.activeIndex]
+		},
+		nextSlide () {
+			return this.speakerMode && this.slides[this.activeIndex + 1]
 		},
 		style () {
 			return {
@@ -35,44 +36,40 @@ export default {
 				'--slides-active': this.activeIndex
 			}
 		},
-		slidesStyle () {
-			if (!this.slides) return
-			return {
-				transform: `scale(${this.slideScale})`
-			}
-		}
+
+	},
+	watch: {
+		activeIndex: 'sendState'
 	},
 	created () {
 	},
 	mounted () {
-		const children = Array.from(this.$refs.slides.children)
+		const children = Array.from(this.$el.children)
 		this.slides = this.$children.filter(slide => slide._isSlide).sort((a, b) => children.indexOf(a.$el) - children.indexOf(b.$el))
 
 		document.addEventListener('keydown', this.globalKeyHandler)
-		window.addEventListener('resize', this.computeScale)
+		window.addEventListener('message', this.globalMessageHandler)
+
+		// report to parent
+		window.opener?.postMessage(['loaded'])
 	},
 	beforeDestroy () {
 		document.removeEventListener('keydown', this.globalKeyHandler)
-		window.removeEventListener('resize', this.computeScale)
 	},
 	methods: {
-		computeScale () {
-			const width = this.$el.offsetWidth
-			const height = this.$el.offsetHeight
-			this.slideScale = Math.min(width / SLIDE_WIDTH, height / SLIDE_HEIGHT)
-		},
 		changeSlide (newIndex) {
-			if (!this.slides[newIndex]) return
-			this.activeSlide.$once('after-leave', () => {
+			if (!this.slides[newIndex] || this.activeIndex === newIndex) return
+			/* this.activeSlide.$once('after-leave', () => {
 				this.animating = false
 			})
-			this.animating = true
+			this.animating = true */
 			this.activeIndex = newIndex
 		},
 		next () {
 			if (this.activeSlide.shownFragments < this.activeSlide.fragments.length) {
 				this.activeSlide.showNextFragment()
 			} else {
+				this.transitionDirection = 'next'
 				this.changeSlide(this.activeIndex + 1)
 			}
 		},
@@ -80,7 +77,13 @@ export default {
 			if (this.activeSlide.shownFragments > 0) {
 				this.activeSlide.showPreviousFragment()
 			} else {
+				this.transitionDirection = 'previous'
 				this.changeSlide(this.activeIndex - 1)
+			}
+		},
+		toggleSpeakerMode () {
+			if (!this.speakerMode) {
+				this._speakerWindow = window.open('/', 'Daumenkino Speaker Mode', 'height=700,width=960')
 			}
 		},
 		globalKeyHandler (event) {
@@ -95,13 +98,44 @@ export default {
 					this.previous()
 					break
 				}
+				case 's':
+					this.toggleSpeakerMode()
+					break
 			}
+		},
+		// Speaker Mode Methods
+		globalMessageHandler (event) {
+			switch (event.data[0]) {
+				case 'loaded': {
+					this._speakerWindow.postMessage(['initSpeakerMode'])
+					this.sendState()
+					break
+				}
+				case 'initSpeakerMode': {
+					this.speakerMode = true
+					break
+				}
+				case 'updateState': {
+					this.changeSlide(event.data[1].activeIndex)
+					this.activeSlide.updateShownFragments(event.data[1].shownFragments)
+					break
+				}
+			}
+		},
+		sendState () {
+			const otherWindow = this._speakerWindow || (this.speakerMode && window.opener)
+			if (!otherWindow) return
+			otherWindow.postMessage(['updateState', {
+				activeIndex: this.activeIndex,
+				shownFragments: this.activeSlide.shownFragments
+			}])
 		}
 	}
 }
 </script>
 <style lang="stylus">
 .daumenkino
+	position: relative
 	height: 100vh
 	width: 100vw
 	display: flex
@@ -110,8 +144,6 @@ export default {
 	align-items: center
 	overflow: hidden
 	.slides
-		width: 960px
-		height: 700px
 		flex: none
 		display: flex
 		justify-content: center
@@ -128,11 +160,4 @@ export default {
 			height: 4px
 			width: calc((var(--slides-active) + 1) / var(--slides-total) * 100%)
 			background-color: red
-
-.slide-next-enter-active, .slide-next-leave-active
-	transition: opacity .3s
-
-.slide-next-enter, .slide-next-leave-to
-	opacity: 0
-
 </style>
--- src/slide.vue
+++ src/slide.vue
@@ -1,20 +1,43 @@
 <template lang="pug">
-transition(name="slide-next", @after-leave="afterLeave")
-	.daumenkino-slide(v-show="active")
+transition(:name="`slide-${direction}`", @after-leave="afterLeave")
+	.daumenkino-slide(v-show="active || next", :class="{next}", :style="style")
 		slot
 </template>
 <script>
+const SLIDE_WIDTH = 960
+const SLIDE_HEIGHT = 700
 export default {
 	components: {},
 	data () {
 		return {
 			fragments: [],
-			shownFragments: 0
+			showFragments: 0,
+			scale: 1
 		}
 	},
 	computed: {
 		active () {
 			return !this.$parent.animating && this.$parent.activeSlide === this
+		},
+		next () {
+			return this.$parent.nextSlide === this
+		},
+		style () {
+			const speakerFactor = this.$parent.speakerMode ? (this.next ? 0.5 : 0.5) : 1
+			return {
+				'--scale': this.scale * speakerFactor
+			}
+		},
+		shownFragments () {
+			return this.next ? this.fragments.length : this.showFragments
+		},
+		direction () {
+			return this.$parent.transitionDirection
+		}
+	},
+	watch: {
+		shownFragments () {
+			this.$parent.sendState()
 		}
 	},
 	created () {
@@ -36,23 +59,61 @@ export default {
 		}
 		orderByDOM(this.$el.children)
 		this.fragments = fragments
+
+		window.addEventListener('resize', this.computeScale)
+		this.computeScale()
+	},
+	beforeDestroy () {
+		window.removeEventListener('resize', this.computeScale)
 	},
 	methods: {
+		computeScale () {
+			const width = document.body.offsetWidth
+			const height = document.body.offsetHeight
+			this.scale = Math.min(width / SLIDE_WIDTH, height / SLIDE_HEIGHT)
+		},
 		afterLeave () {
 			this.$emit('after-leave')
 		},
+		updateShownFragments (showFragments) {
+			this.showFragments = showFragments
+		},
 		showNextFragment () {
-			this.shownFragments++
+			this.showFragments++
 		},
 		showPreviousFragment () {
-			this.shownFragments--
+			this.showFragments--
 		}
 	}
 }
 </script>
 <style lang="stylus">
 .daumenkino-slide
-
+	position: absolute
+	width: 960px
+	height: 700px
+	display: flex
+	flex-direction: column
+	justify-content: center
+	align-items: center
+	transform: scale(var(--scale)) translateZ(0)
 	.daumenkino-fragment:not(.daumenkino-fragment-show)
 		visibility: hidden
+.speaker-mode
+	.daumenkino-slide
+		border: 2px solid #CCC
+		top: calc(16px - 350px + 350px * var(--scale))
+		&:not(.next)
+			left: calc(16px - 480px + 480px * var(--scale))
+		&.next
+			right: calc(16px - 480px + 480px * var(--scale))
+
+.daumenkino:not(.speaker-mode)
+	.slide-next-enter-active, .slide-next-leave-active, .slide-previous-enter-active, .slide-previous-leave-active
+		transition: transform .5s ease
+
+	.slide-next-enter, .slide-previous-leave-to
+		transform: translateX(100vw)
+	.slide-next-leave-to, .slide-previous-enter
+		transform: translateX(-100vw)
 </style>
